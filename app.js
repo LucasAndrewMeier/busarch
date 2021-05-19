9584c366369cc1d9bde6cf837cac151db29d6422
Lucas Meier <meiluca@dunwoody.edu>
2021-05-19 11:17:31 -0500

    working

---
+++ app.js
@@ -0,0 +1,54 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var createuserscriptRouter = require('./routes/createuserscript');
+var displayRouter = require('./routes/display');
+
+var createUserRouter = require('./routes/createuser');
+
+
+var loginRouter = require('./routes/login');
+
+var app = express();
+
+var path = require('path')
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
+
+app.use('/', indexRouter);
+app.use('/createuser', createUserRouter);
+app.use('/adduser', createuserscriptRouter);
+app.use('/display', displayRouter);
+
+app.use('/login',loginRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
